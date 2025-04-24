05e10f26c2d78274decd66b81c0f724fa501030d
Brion <info@brionmario.com>
2025-04-24 11:47:33 +0530

    chore: restructure build

--- packages/react/src/components/Tabs/Tabs.tsx
+++ packages/react/src/components/Tabs/Tabs.tsx
@@ -19,10 +19,11 @@
 import MuiTabs from '@mui/material/Tabs';
 import type {TabsProps as MuiTabsProps, TabsTypeMap} from '@mui/material/Tabs';
 import clsx from 'clsx';
-import {forwardRef, ForwardRefExoticComponent} from 'react';
-import type {ElementType, Ref, ReactElement} from 'react';
+import {forwardRef} from 'react';
+import type {ElementType, ReactElement} from 'react';
 import Box from '../Box';
 import Divider from '../Divider';
+import { OverridableComponent } from '@mui/material/OverridableComponent';
 
 export type TabsProps<
   C extends ElementType = ElementType,
@@ -52,29 +53,25 @@ export type TabsProps<
  * - ✅ `component` prop is supported.
  * - ✅ The `ref` is forwarded to the root element.
  *
- * @deprecated Use the `Tab` component directly from `@oxygen-ui/react/Tab` instead.
- * Tracker: https://github.com/wso2/oxygen-ui/issues/340
  *
  * @template C - The type of the component.
  * @param props - The props for the Skeleton component.
  * @param ref - The ref to be forwarded to the MuiSkeleton component.
  * @returns The rendered Skeleton component.
  */
-const Tabs: ForwardRefExoticComponent<TabsProps> = forwardRef(
-  <C extends ElementType = ElementType>({className, ...rest}: TabsProps<C>, ref: Ref<HTMLDivElement>): ReactElement => (
-    <Box
-      className={clsx(
-        /* @deprecated Use the PascalCase classname instead. https://github.com/wso2/oxygen-ui/issues/274 */
-        'oxygen-tabs',
-        'OxygenTabs-root',
-        className,
-      )}
-    >
-      {/* @ts-ignore */}
-      <MuiTabs ref={ref} {...rest} />
-      <Divider />
-    </Box>
-  ),
-) as unknown as ForwardRefExoticComponent<TabsProps>;
+const Tabs: OverridableComponent<TabsTypeMap<TabsProps>> = forwardRef(
+  <C extends ElementType = ElementType>({className, ...rest}: TabsProps<C>, ref: any): ReactElement => {
+    const classes: string = clsx('oxygen-tabs', className);
+
+    // TODO: Need to evaluate this wrapper.
+    // Should directly return the MuiTabs component instead of wrapping it with Box and Divider.
+    return (
+      <Box className={classes}>
+        <MuiTabs ref={ref} {...rest} />
+        <Divider />
+      </Box>
+    );
+  },
+) as OverridableComponent<TabsTypeMap<TabsProps>>;
 
 export default Tabs;
